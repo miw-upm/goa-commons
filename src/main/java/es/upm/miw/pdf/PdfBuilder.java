ff32a6f9c997ed6f7eaf0f0116a07b31708f0c63
JBernal <j.bernal@upm.es>
2026-04-23 20:03:16 +0200

    version>1.5.10. miw-upm/goa#148

--- src/main/java/es/upm/miw/pdf/PdfBuilder.java
+++ src/main/java/es/upm/miw/pdf/PdfBuilder.java
@@ -19,8 +19,8 @@ public class PdfBuilder {
     private static final float PAGE_MARGIN_LEFT_RIGHT = 36f;
     private static final float PAGE_MARGIN_TOP = 22f;
     private static final float PAGE_MARGIN_BOTTOM = 70f;
-    private static final float FOOTER_TEXT_OFFSET = 27f;
-    private static final float FOOTER_SEPARATOR_OFFSET = 8f;
+    private static final float FOOTER_TEXT_OFFSET = 42f;
+    private static final float FOOTER_SEPARATOR_OFFSET = 20f;
 
     private static final BaseFont BASE_FONT_HELVETICA = requiredBaseFont(BaseFont.HELVETICA);
     private static final BaseFont BASE_FONT_HELVETICA_BOLD = requiredBaseFont(BaseFont.HELVETICA_BOLD);
