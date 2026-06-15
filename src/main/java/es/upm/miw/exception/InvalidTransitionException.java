561a5d2a5757b2b476008e72ae5bbc6fba82f853
JBernal <j.bernal@upm.es>
2026-06-15 11:15:12 +0200

    fixed exceptions

--- src/main/java/es/upm/miw/exception/InvalidTransitionException.java
+++
@@ -1,7 +0,0 @@
-package es.upm.miw.exception;
-
-public class InvalidTransitionException extends BusinessRuleException {
-    public InvalidTransitionException(String userMessage) {
-        super(userMessage);
-    }
-}
